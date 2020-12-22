83cf1d9db18f8af1061a946972a2db7dc2d8a9a3
Karim Stekelenburg <karim@animo.id>
2020-12-22 01:07:18 +0100

    Add RNCamera to ScannerView
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- src/views/ScannerView.tsx
+++ src/views/ScannerView.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-
 import { Layout, Text } from '@ui-kitten/components';
 
+import { RNCamera } from 'react-native-camera';
+
 const ScannerView = () => {
+  let camera: RNCamera = null;
+
   return (
     <Layout style={styles.container}>
       <Text category="h5">QR Scanner</Text>
+      <RNCamera
+        ref={ref => {
+          camera = ref;
+        }}
+        style={{
+          flex: 1,
+          width: '100%',
+        }}
+      />
     </Layout>
   );
 };
